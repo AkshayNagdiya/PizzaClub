38abd694b2a0cd229f39bf54c9d294d540c0d2b8
Akshay Nagdiya <nagdiyaakshay@gmail.com>
2024-05-11 03:37:21 +0530

    Button Color

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -60,13 +60,13 @@ const CartItem = ({ cart }) => {
               <p>Quantity</p>
               <span>
                 <div
-                  className="sm:p-1 inline-block bg-white border border-gray-200 rounded-lg dark:bg--900 dark:border-gray-700"
+                  className="sm:p-1 inline-block border border-gray-200 rounded-lg dark:bg--900 dark:border-gray-700"
                   data-hs-input-number
                 >
                   <div className="flex items-center gap-x-1.5">
                     <button
                       type="button"
-                      className=" size-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                      className=" size-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                       data-hs-input-number-decrement
                       onClick={() => handleDecrement(id)}
                     >
@@ -90,7 +90,7 @@ const CartItem = ({ cart }) => {
                     </p>
                     <button
                       type="button"
-                      className="size-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                      className="size-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                       data-hs-input-number-increment
                       onClick={() => handleIncrement(id)}
                     >
